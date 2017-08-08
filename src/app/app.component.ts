bde4bd11d7172032800634e63fff05da87385e87
Daniel Olano <olanoda@amer.net.intra>
2017-08-08 17:45:20 -0500

    show hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+	id: number;
+	name: string;
+}
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: 
+  `
+  <h1>{{title}}</h1>
+  <h2>{{hero.name}} details!</h2>
+  <div>id: {{hero.id}}</div>
+  <div>name: {{hero.name}}</div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  hero: Hero = {
+  	id: 1,
+  	name: 'Iron Fist'
+  };
 }

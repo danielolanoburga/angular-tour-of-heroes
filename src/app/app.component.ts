3a57f52c22f99c097b1db3dce3c4a1dd1d8363a0
Daniel Olano <olanoda@amer.net.intra>
2017-08-09 10:19:19 -0500

    show list of heroes with css styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,8 +19,65 @@ export class Hero {
   	<label>name: {{hero.name}}</label>
   	<input [(ngModel)]="hero.name" placeholder="name">
   </div>
+  <br>
+  <h2>My Heroes</h2>
+  <ul class="heroes">
+  	<li *ngFor="let hero of heroes"> 
+  		<span class="badge">{{hero.id}}</span> {{hero.name}}
+  	</li>
+  </ul>
+
   `,
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+	.selected {
+		background-color: #CFD8DC !important;
+		color: white;
+	}
+	.heroes {
+		margin: 0 0 2em 0;
+		list-style-type: none;
+		padding: 0;
+		width: 15em;
+	}
+	.heroes li {
+		cursor: pointer;
+		position: relative;
+		left: 0;
+		background-color: #EEE;
+		margin: .5em;
+		padding: .3em 0;
+		height: 1.6em;
+		border-radius: 4px;
+	}
+	.heroes li.selected:hover {
+		background-color: #BBD8DC !important;
+		color: white;
+	}
+	.heroes li:hover {
+		color: #607D8B;
+		background-color: #DDD;
+		left: .1em;
+	}
+	.heroes .text {
+		position: relative;
+		top: -3px;
+	}
+	.heroes .badge {
+		display: inline-block;
+		font-size: small;
+		color: white;
+		padding: 0.8em 0.7em 0 0.7em;
+		background-color: #607D8B;
+		line-height: 1em;
+		position: relative;
+		left: -1px;
+		top: -4px;
+		height: 1.8em;
+		margin-right: .8em;
+		border-radius: 4px 0 0 4px;
+	}`
+   ]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
@@ -28,4 +85,12 @@ export class AppComponent {
   	id: 1,
   	name: 'Iron Fist'
   };
+  heroes = HEROES;
 }
+
+const HEROES: Hero[] = [
+	{id: 11, name: 'Iron Fist'},
+	{id: 12, name: 'Luke Cage'},
+	{id: 13, name: 'Jessica Jones'},
+	{id: 14, name: 'Dared Devil'}
+];
